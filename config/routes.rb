9d0518363789757c55a314f6e5e3b1e704c85441
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2016-04-07 13:48:28 +0200

    Added get route to search#create

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Ecm::Tags::Engine.routes.draw do
-  resources :tag_searchs, only: [:new, :create]
+  resources :tag_searchs, only: [:new, :create] do
+    get '/', on: :collection, to: "tag_searchs#create"
+  end
 
   root to: 'tag_searchs#new'
 end
