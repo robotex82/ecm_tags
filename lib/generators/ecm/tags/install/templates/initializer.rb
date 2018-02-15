497a80aafc6c58ff97cee1126f7806b918d4cf0d
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2018-02-15 21:54:21 +0100

    Make base controller class name configurable in the install generator via BASE_CONTROLLER_CLASS_NAME option. Fix install generator using wrong mount point when generating routes.

--- lib/generators/ecm/tags/install/templates/initializer.rb
+++ lib/generators/ecm/tags/install/templates/initializer.rb
@@ -1,9 +1,9 @@
 Ecm::Tags.configure do |config|
   # Set the base controller
   #
-  # Default: config.base_controller = 'FrontendController'
+  # Default: config.base_controller = '<%= base_controller_class_name %>'
   #
-  config.base_controller = 'FrontendController'
+  config.base_controller = '<%= base_controller_class_name %>'
 
   # Add a submit button to the tag search box.
   # 
